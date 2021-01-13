a83fb7fdae232aa848ccd58f1ca0a3349f77c529
georginarojas <ginarojas29@gmail.com>
2021-01-12 23:10:46 -0300

    Formatting the back and home buttons

--- appgoals/Client/src/components/utils/goBack.js
+++ appgoals/Client/src/components/utils/goBack.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
-  function GoBack(){
-    const history = useHistory();
-    return (
-      <div>
-          <button
-            type="button"
-            className="button-goBack"
-            onClick={() => {
-              history.goBack();
-            }}
-          >
-            Go back
-          </button>
-      </div>
-    );
+function GoBack() {
+  const history = useHistory();
+  return (
+    <div className="utils-button">
+      <button
+        type="button"
+        className="button-goBack"
+        onClick={() => {
+          history.goBack();
+        }}
+      >
+        <FontAwesomeIcon icon={faArrowLeft} />
+      </button>
+    </div>
+  );
 }
 
 export default GoBack;
--- appgoals/Client/src/components/utils/goHome.js
+++ appgoals/Client/src/components/utils/goHome.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 class GoHome extends Component {
   render() {
     return (
-      <div>
+      <div >
         <Link to="/home">
-          <button type="button" className="button-goHome">
-           Go Home
+          <button type="button" className="utils-button">
+            <FontAwesomeIcon icon={faHome} color="blue" size="lg"/>
           </button>
         </Link>
       </div>
