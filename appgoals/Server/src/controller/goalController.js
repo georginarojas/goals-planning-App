484c8ddbe302ca9ae8eccf5295cf7ec9bd04e5d3
georginarojas <ginarojas29@gmail.com>
2021-01-26 17:33:52 -0300

    Adding the module find in controllerGoal, this module load the goal and its missions. For this was used "agregate and lookup".

--- appgoals/Server/src/controller/goalController.js
+++ appgoals/Server/src/controller/goalController.js
@@ -2,36 +2,103 @@ const mongoose = require("mongoose");
 const Goal = mongoose.model("Goal");
 
 module.exports = {
+  //*********************//
+  //------- Index -------//
+  //*********************//
   async index(req, res) {
-    const goals = await Goal.find({ userId: req.body.userId });
-    return res.json(goals);
-  },
-
-  async find(req, res) {
-    const goal = await Goal.findOne({ userId: req.body.userId })
-      .populate("userId", "name")
-      .exec((error, goal) => {
-        if (error) {
-          return handleError(error);
-        }
-        console.log("Goal user name ", goal.userId.name);
-        console.log("Goal age user ", goal.userId.age);
-        console.log(">> GOAL ", goal);
+    try {
+      const goals = await Goal.find();
+      return res.status(200).json(goals);
+    } catch (error) {
+      res.status(500).json({
+        status: "failure",
+        error: error.message,
       });
-    // console.log(">> GOAL ", goal);
-    return res.json(goal);
+    }
   },
-
+  //*********************************************//
+  //------- Find a goal and its missions--------//
+  //********************************************//
+  async find(req, res) {
+    try {
+      const goal = await Goal.findById(req.params.id);
+      if (goal !== null) {
+        const goalWithMissions = await Goal.aggregate([
+          {
+            $match: { _id: mongoose.Types.ObjectId(req.params.id) },
+          },
+          {
+            $lookup: {
+              from: "missions",
+              localField: "_id",
+              foreignField: "goalId",
+              as: "missions",
+            },
+          },
+        ]);
+        return res.status(200).json({
+          status: "success",
+          data: goalWithMissions,
+        });
+      } else {
+        return res.status(404).json({
+          status: "failure",
+          data: null,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        status: "failure",
+        error: error.message,
+      });
+    }
+  },
+  //*******************************//
+  //------- Save a new goal -------//
+  //*******************************//
   async store(req, res) {
-    console.log("Entrei");
-    const goal = await Goal.create(req.body);
-    console.log(">> GOAL ", goal);
-    return res.json(goal);
+    try {
+      const goal = await Goal.create(req.body);
+      if (goal !== null) {
+        return res.status(201).json({
+          status: "success",
+          data: goal,
+        });
+      } else {
+        return res.status(400).json({
+          status: "failure",
+          data: null,
+        });
+      }
+      return res.json(goal);
+    } catch (error) {
+      res.status(500).json({
+        status: "failure",
+        error: error.message,
+      });
+    }
   },
 
-  async delete(req, res){
-      console.log("Delete id ", req.body._id)
-      const goal = await Goal.findByIdAndRemove(req.body._id);
-      return res.json(goal);
-  }
+  //*********************//
+  //------- Delete-------//
+  //*********************//
+  async delete(req, res) {
+    try {
+      const goal = await Goal.findByIdAndRemove(req.params.id);
+      if (goal !== null) {
+        return res.status(200).json({
+          status: "success",
+        });
+      } else {
+        return res.status(404).json({
+          status: "failure",
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        status: "failure",
+        error: error.message,
+      });
+    }
+  },
 };
--- appgoals/Server/src/models/goal.js
+++ appgoals/Server/src/models/goal.js
@@ -9,6 +9,7 @@ const GoalScheme = new mongoose.Schema(
     userId: { 
         type: mongoose.Schema.Types.ObjectId, 
         ref: "User", 
+        required: true,
     },
   },
   {
