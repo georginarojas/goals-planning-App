cf40315e28ededf7febfa004634b094186650aef
georginarojas <ginarojas29@gmail.com>
2021-01-26 17:32:31 -0300

    Adding the module find in controllerMission, this module load the mission and its tasks. For this was used "agregate and lookup".

--- appgoals/Server/src/controller/missionController.js
+++ appgoals/Server/src/controller/missionController.js
@@ -2,10 +2,104 @@ const mongoose = require("mongoose");
 const Mission = mongoose.model("Mission");
 
 module.exports = {
-
-    async store(req, res) {
-        const mission = await Mission.create(req.body);
-        return res.json(mission);
+  //*********************//
+  //------- Index -------//
+  //*********************//
+  async index(req, res) {
+    try {
+      const mission = await Mission.find();
+      return res.status(200).json(mission);
+    } catch (error) {
+      res.status(500).json({
+        status: "failure",
+        error: error.message,
+      });
     }
+  },
 
+  //*******************************//
+  //----- Save a new mission ------//
+  //*******************************//
+  async store(req, res) {
+    try {
+      const mission = await Mission.create(req.body);
+      if (mission !== null) {
+        return res.status(201).json({
+          status: "success",
+          data: mission,
+        });
+      } else {
+        return res.status(400).json({
+          status: "failure",
+          data: null,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        status: "failure",
+        error: error.message,
+      });
+    }
+  },
+  //*********************************************//
+  //------- Find a mission and its tasks --------//
+  //********************************************//
+  async find(req, res) {
+    try {
+      const mission = await Mission.findById(req.params.id);
+      console.log("MISSION REQ", mission);
+      if (mission !== null) {
+        const missionWithTasks = await Mission.aggregate([
+          {
+            $match: { _id: mongoose.Types.ObjectId(req.params.id) },
+          },
+          {
+            $lookup: {
+              from: "tasks",
+              localField: "_id",
+              foreignField: "missionId",
+              as: "tasks",
+            },
+          },
+        ]);
+        return res.status(200).json({
+          status: "success",
+          data: missionWithTasks,
+        });
+      } else {
+        return res.status(400).json({
+          status: "failure",
+          data: null,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        status: "failure",
+        error: error.message,
+      });
+    }
+  },
+
+  //*********************//
+  //------- Delete-------//
+  //*********************//
+  async delete(req, res) {
+    try {
+      const mission = await Mission.findByIdAndRemove(req.params.id);
+      if (mission !== null) {
+        return res.status(200).json({
+            status: "success",
+        });
+      } else {
+        return res.status(404).json({
+            status: "failure",
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        status: "failure",
+        error: error.message,
+      });
+    }
+  },
 };
--- appgoals/Server/src/models/mission.js
+++ appgoals/Server/src/models/mission.js
@@ -9,18 +9,10 @@ const MissionScheme = new mongoose.Schema(
     goalId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Goal",
+      required: true,
     },
   },
   { timestamps: true }
 );
 
 mongoose.model("Mission", MissionScheme);
-
-// {
-//     "_id" : ObjectId("600dfe3ec740f255b8f116f3"),
-//     "title" : "Work out 5",
-//     "userId" : ObjectId("5fd95ca235ecdf5994b5edfb"),
-//     "createdAt" : ISODate("2021-01-24T23:09:50.391Z"),
-//     "updatedAt" : ISODate("2021-01-24T23:09:50.391Z"),
-//     "__v" : 0
-// }
