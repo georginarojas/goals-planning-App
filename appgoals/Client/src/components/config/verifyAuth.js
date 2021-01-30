1b1aae93c901bc85b1ba49339659d3ca0d1dae9c
georginarojas <ginarojas29@gmail.com>
2021-01-30 18:27:30 -0300

    Improvement the verifyAuth file. Was created functions and a better organization for to using in the other components and pages.

--- appgoals/Client/src/components/config/verifyAuth.js
+++ appgoals/Client/src/components/config/verifyAuth.js
@@ -1,45 +1,73 @@
-import React, { useEffect } from "react";
-import api from "../../services/api";
-import { withRouter } from "react-router-dom";
+import React from "react";
+import jwt_decode from "jwt-decode";
 
-const VerifyAuth = (props) => {
-  const auth = localStorage.getItem("Auth");
-  if (!auth) {
+export const login = (props, token, auth, user) => {
+  console.log("VERIFY Login ", token, auth, user, props);
+  if (auth) {
+    localStorage.setItem("JWT", token);
+    localStorage.setItem("Auth", auth);
+    localStorage.setItem("User", JSON.stringify(user));
+    props.setContext({
+      auth: auth,
+      user: user,
+    });
+
+    props.history.push("/home");
+    return true;
+  } else {
+    props.props.setContext({ auth: false, user: null });
     props.history.push("/login");
+    return false;
   }
-
-  const jwt = localStorage.getItem("JWT");
-  const user = JSON.parse(localStorage.getItem("User"));
-
-  useEffect(() => {
-    if (jwt !== null) {
-      getUser();
-    } else {
-      props.props.setContext({ auth: false, user: null });
-    }
-  }, []);
-
-  const getUser = async () => {
-    try {
-      const userId = user._id;
-      const response = await api.get("/auth", {
-        params: { userId },
-        headers: { Authorization: `JWT ${jwt}` },
-      });
-      // console.log(">>>>>> VERIFY RESponse ", response);
-      localStorage.setItem("Auth", response.data.auth);
-      props.props.setContext({
-        auth: response.data.auth,
-        user: response.data.data,
-      });
-    } catch (error) {
-      // console.log("VERIFY error ", error);
-      localStorage.clear();
-      props.history.push("/login");
-      props.props.setContext({ auth: false, user: null });
-    }
-  };
-  return <div></div>;
 };
 
-export default withRouter(VerifyAuth);
+export const isLogin = (props) => {
+  const jwt = localStorage.getItem("JWT");
+  if (jwt !== null) {
+    const decodedJwt = jwt_decode(jwt);
+
+    let currrentDate = new Date();
+    console.log("Time expery ", decodedJwt.exp * 1000, currrentDate.getTime());
+    if (decodedJwt.exp * 1000 < currrentDate.getTime()) {
+      console.log("VERIFy isLogin ", false);
+      sessionExpired(props);
+      return false;
+    } else {
+      console.log("VERIFy isLogin ", true);
+      return true;
+    }
+  } else {
+    console.log("VERIFy isLogin null ");
+
+    sessionExpired(props);
+    return false;
+  }
+};
+
+export const sessionExpired = (props) => {
+  localStorage.clear();
+  let message = "Session expired";
+  if (props !== null) {
+    console.log(">>> Session expired ", props);
+    props.setContext({ auth: false, user: null });
+    props.history.push("/login");
+  }
+  return message;
+};
+
+export const logout = (props) => {
+  console.log("Logout");
+  localStorage.clear();
+  props.history.push("/");
+  let message = "Logout success";
+  return message;
+};
+
+export const intervalTime = () => {
+  let oneMinute = 1000 * 60, // milliseconds
+      minutes = 1;
+
+  let time = oneMinute * minutes + 1000; 
+  return time;
+};
+
